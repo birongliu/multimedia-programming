dc41b4b00c8010b531b2bc5bda355f684656d75c
birongliu <birongliu2003@gmail.com>
2024-03-04 12:36:19 -0500

    Add fruit lab files and update cartoon project

---
+++ labs/fruit/index.js
@@ -0,0 +1,6 @@
+const fruit = [
+    { name: "Apple", quantity: 20, color: "red" },
+    { name: "Orangle", quantity: 10, color: "orange" }
+]
+
+
--- projects/cartoon/index.js
+++ projects/cartoon/index.js
@@ -4,34 +4,61 @@ class Items {
   constructor(canvas, context) {
     this.ctx = context;
     this.canvas = canvas;
-    this.spaceShipData = {
-      color: "blue",
-      width: 8,
-      height: 22,
-      position: {
-        x: 0,
-        y: 0,
-      },
-      angle: 0,
-    };
+  }
+
+  grass() {
+    ctx.beginPath();
+    ctx.moveTo(75, 50);
+    ctx.lineTo(100, 75);
+    ctx.lineTo(100, 25);
+    ctx.fill();
+  }
+
+  house() {
+    this.ctx.save();
+    this.ctx.beginPath();
+    this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
+    this.ctx.fillStyle = "yellow";
+    this.ctx.rect(0, 0, 100 / 2, -50 / 2);
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(10, 0);
+    this.ctx.lineTo(10, -10);
+    this.ctx.lineTo(0, -10);
+    this.ctx.lineTo(0, 0);
+    this.ctx.fillStyle = "brown";
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(25.5, -40);
+    this.ctx.lineTo(0, -25);
+    this.ctx.lineTo(50, -25);
+    this.ctx.fillStyle = "brown";
+    this.ctx.fill();
+    this.ctx.closePath();
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(40, -10);
+    this.ctx.lineTo(30, -10);
+    this.ctx.lineTo(30, -2);
+    this.ctx.lineTo(40, -2);
+    this.ctx.lineTo(40, -11);
+    this.ctx.fillStyle = "white";
+    this.ctx.fill();
+    this.ctx.stroke();
   }
 
   moon(x, y) {
+    this.ctx.save();
     this.ctx.beginPath();
     this.ctx.fillStyle = "white";
     this.ctx.arc(x, y, 12.5, 0, 2 * Math.PI);
     this.ctx.fill();
     this.ctx.closePath();
-  }
-
-  spaceShip() {
-    this.ctx.save();
-    this.ctx.beginPath();
-    this.ctx.translate(this.canvas.width / 2, this.canvas.height / 2);
-    this.ctx.arc(0, 0, 50, 0, 2 * Math.PI / 2)
-    this.ctx.fillStyle = this.spaceShipData.color;
-    this.ctx.fill();
-    this.ctx.closePath()
+    this.ctx.restore();
   }
 
   star() {
@@ -50,7 +77,7 @@ class Items {
       this.ctx.closePath();
       this.ctx.fillStyle = "#bbb";
       this.ctx.fill();
-      this.ctx.closePath()
+      this.ctx.closePath();
     }
   }
 }
@@ -63,6 +90,7 @@ class Cartoon {
     night: "#5c54a4",
     ground: "#e9bf83",
   };
+
   setTitle(title, x, y) {
     this.context.font = "18px serif";
     this.context.fillStyle = "blue";
@@ -93,7 +121,7 @@ class Cartoon {
       0,
       this.canvas.height / 2,
       this.canvas.width,
-      this.canvas.height / 2
+      this.canvas.height
     );
     return this;
   }
@@ -101,9 +129,9 @@ class Cartoon {
   draw() {
     this.items.moon(30, 20);
     this.items.star();
-    this.items.spaceShip()
+    this.items.house();
+    this.items.grass();
   }
 }
 
 new Cartoon().setUp().draw();
-
