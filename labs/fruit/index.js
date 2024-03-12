9c56bae6bac91e911841f269d7c775a3b2d70beb
birongliu <birongliu2003@gmail.com>
2024-03-12 13:02:03 -0400

    Update HTML and JavaScript code for fruit lab

--- labs/fruit/index.js
+++ labs/fruit/index.js
@@ -1,6 +1,45 @@
-const fruit = [
-    { name: "Apple", quantity: 20, color: "red" },
-    { name: "Orangle", quantity: 10, color: "orange" }
-]
+const canvasDocs = document.getElementById("canvas");
+const json = [
+  {
+    name: "Apple",
+    quantity: 20,
+    color: "red",
+  },
+  {
+    name: "Orange",
+    quantity: 10,
+    color: "orange",
+  },
+  {
+    name: "Banana",
+    quantity: 15,
+    color: "yellow",
+  },
+  {
+    name: "Kiwi",
+    quantity: 5,
+    color: "green",
+  },
+  {
+    name: "Blueberry",
+    quantity: 5,
+    color: "blue",
+  },
+  {
+    name: "Grapes",
+    quantity: 10,
+    color: "blue",
+  },
+];
 
-
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let height = 0;
+for (let i = 0; i < json.length; i++) {
+  ctx.fillStyle = json[i].color;
+  ctx.fillRect(0, 10 + height, 14 * (json[i].quantity), 15);
+  ctx.fillStyle = "black"
+  ctx.font = "10px"
+  ctx.fillText(`${json[i].quantity} ${json[i].name}`, 2, (height + 20))
+  height += 20;
+}
