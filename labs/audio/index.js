a89b832e7daf61ce120faede5310f3bb5cf5198a
birongliu <birongliu2003@gmail.com>
2024-04-09 17:02:54 -0400

    Update audio lab files with new instrument images and music sources

--- labs/audio/index.js
+++ labs/audio/index.js
@@ -1,10 +1,42 @@
-const musicPlayer = document.getElementById("sourceMusicPlayer");
-const imageMap = document.getElementById("sourceImage");
+const musicInstrumentOnePlayer = document.getElementById("music-one");
+const musicInstrumentTwoPlayer = document.getElementById("music-two");
+const musicInstrumentThreePlayer = document.getElementById("music-three");
+const musicInstrumentFourPlayer = document.getElementById("music-four");
+const musicInstrumentFivePlayer = document.getElementById("music-five");
 
-imageMap.onclick = async (evt) => {
+const instrumentOne = document.getElementById("instrument-one");
+const instrumentTwo = document.getElementById("instrument-two");
+const instrumentThree = document.getElementById("instrument-three");
+const instrumentFour = document.getElementById("instrument-four");
+const instrumentFive = document.getElementById("instrument-five");
+
+instrumentOne.onclick = async (evt) => {
   evt.preventDefault();
-  console.log("hello");
+  playOrPauseSong(musicInstrumentOnePlayer)
+};
+
+instrumentTwo.onclick = async (evt) => {
+  evt.preventDefault();
+  playOrPauseSong(musicInstrumentTwoPlayer)
+}
+
+instrumentThree.onclick = async (evt) => {
+  evt.preventDefault();
+  playOrPauseSong(musicInstrumentThreePlayer)
+}
+
+instrumentFour.onclick = async (evt) => {
+  evt.preventDefault();
+  playOrPauseSong(musicInstrumentFourPlayer)
+}
+
+instrumentFive.onclick = async (evt) => {
+  evt.preventDefault();
+  playOrPauseSong(musicInstrumentFivePlayer)
+}
+
+async function playOrPauseSong(musicPlayer) {
   return musicPlayer.paused
     ? await musicPlayer.play()
     : await musicPlayer.pause();
-};
+}
